b61e0b3054e760ea8025469708fb5fea867cf949
Nikita <Libino123@yandex.ru>
2023-04-20 21:04:04 +0600

    Added indent on lines 8-9

--- 3rdTask.java
+++ 3rdTask.java
@@ -4,9 +4,9 @@ class ThirdTask {
     public static void main(String[] args) {
         for (int i = 2; i <= 1000; i++) {
             boolean isPrime = i > 2;
-        for (int j = 2; j < i; j++) {
-            if(i % j == 0){ isPrime = false;
-                break;}
+            for (int j = 2; j < i; j++) {
+                if(i % j == 0){ isPrime = false;
+                    break;}
         }
         if(isPrime)
             System.out.println(i);
